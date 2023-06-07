03c00fd6c8a1154c7b8cc9f3ab599dfaf3b02ddc
cleveng <cleveng@gmail.com>
2023-06-08 00:47:50 +0800

    update error page template

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,21 +1,17 @@
 @extends('layouts.default')
 @section('content')
     @section('breadcrumb')
-        <li>
-            <a href="">异常反馈</a>
-        </li>
+        <li>异常反馈</li>
     @endsection
-    <div class="shell section-bottom-60 offset-top-10">
-        <div class="range range-center">
-            <div class="cell-md-8">
-                <h1 class="offset-top-45 text-limit">404</h1>
-                <h4 class="offset-top-0 text-limit">抱歉，您访问的页面不存在</h4>
-
-                <p><span class="badge badge-primary">5</span> 秒后跳转</p>
-                <a rel="nofollow" href="{{url(env('APP_URL'))}}" class="offset-top-30 btn btn-primary">返回首页</a>
-                <hr class="divider divider-offset-lg divider-gray">
+    <div class="container space-2-bottom">
+        <div class="row text-center">
+            <div class="col">
+                <h1 class="space-1-top display-1">404</h1>
+                <h4 class="mt-0 text-truncate fs-lg">抱歉，您访问的页面不存在</h4>
+                <div class="mt-3">
+                    <a rel="nofollow" href="{{url(env('APP_URL'))}}" class="btn btn-primary">返回首页</a>
+                </div>
             </div>
-            {{--            @include('components.sidebar')--}}
         </div>
     </div>
 @endsection
