547cc6e78b6ca7075c0f9dc2cc00ff9b55ee324b
cleveng <cleveng@gmail.com>
2023-06-22 10:14:41 +0800

    修改 文章链接 社会化分享新增二维码

--- resources/views/components/social.blade.php
+++ resources/views/components/social.blade.php
@@ -15,6 +15,14 @@
            title="不支持"
            class="text-gray icon icon-xs mdi mdi-plus"></a></li>
     @if(isset($item))
+        @if($item->qrcode)
+            <li><a  data-bs-original-title="微信扫一扫"
+                    data-bs-toggle="popover" data-bs-trigger="hover" title="" data-bs-html="true"
+                    data-bs-content="<img src='{{asset($item->qrcode)}}' alt='' class='img-fluid'>"
+                    href="javascript:;" data-bs-container="body"
+                    data-bs-placement="bottom"
+                   class="text-gray icon icon-xs mdi mdi-wechat"></a></li>
+        @endif
         <li><a href="javascript:;" data-bs-toggle="tooltip" data-bs-placement="right"
                title="阅读数：{{$item->views_count}}"
                class="text-gray icon icon-xs mdi mdi-heart"></a></li>
