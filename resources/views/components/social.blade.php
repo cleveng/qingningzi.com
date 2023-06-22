588b62bd24db739d3cd74a2765d74eddc69f8a0d
cleveng <cleveng@gmail.com>
2023-06-22 11:05:45 +0800

    修改 社会化组件 二维码icon

--- resources/views/components/social.blade.php
+++ resources/views/components/social.blade.php
@@ -16,12 +16,12 @@
            class="text-gray icon icon-xs mdi mdi-plus"></a></li>
     @if(isset($item))
         @if($item->qrcode)
-            <li><a  data-bs-original-title="微信扫一扫"
+            <li><a  data-bs-original-title="手机扫一扫"
                     data-bs-toggle="popover" data-bs-trigger="hover" title="" data-bs-html="true"
                     data-bs-content="<img src='{{asset($item->qrcode)}}' alt='' class='img-fluid'>"
                     href="javascript:;" data-bs-container="body"
                     data-bs-placement="bottom"
-                   class="text-gray icon icon-xs mdi mdi-wechat"></a></li>
+                   class="text-gray icon icon-xs mdi mdi-qrcode"></a></li>
         @endif
         <li><a href="javascript:;" data-bs-toggle="tooltip" data-bs-placement="right"
                title="阅读数：{{$item->views_count}}"
