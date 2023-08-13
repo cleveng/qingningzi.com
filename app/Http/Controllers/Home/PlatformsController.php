f75e3a9b385f2592fc8ef06a9cb34e8c4fc1c5f6
cleveng <cleveng@gmail.com>
2023-08-14 01:09:54 +0800

    update platforms blade content

--- app/Http/Controllers/Home/PlatformsController.php
+++ app/Http/Controllers/Home/PlatformsController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Models\Platform;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PlatformsController extends BaseController
 {
     public function index(Request $request)
     {
-        // TODO: Platforms
-        return redirect(url('/'), 302);
+        $title = "合作媒体";
+        SEOMeta::setTitle($title);
+        SEOMeta::addKeyword($title);
+        SEOMeta::setDescription($title);
+        SEOMeta::setCanonical(url('platforms'));
+
+        $page = $request->has('page') ? $request->get('page') : 1;
+        $data = Cache::remember('platform_' . $page, $this->expiredAt, function () {
+            return Platform::where('status', true)->orderBy('id', 'desc')->paginate($this->prePage);
+        });
+
+        return view('pages.platforms.index', [
+            'title' => $title,
+            'data' => $data,
+        ]);
     }
 }
--- resources/views/pages/platforms/index.blade.php
+++ resources/views/pages/platforms/index.blade.php
@@ -1,71 +1,37 @@
 @extends('layouts.default')
 @section('content')
-    @section('breadcrumb')
-        <li>
-            <a href="{{url($url)}}" rel="nofollow">
-                {{$title}}
-            </a>
-        </li>
-        <li class="active">
-            合作媒体列表(部分有二维码展示)
-        </li>
-    @endsection
-    <div class="container space-2-bottom--lg mt-3">
+    <div class="container space-2">
         <div class="row">
-            @foreach($data as $key=>$media)
-                @if($key == 0)
-                    <div class="offset-top-0 col-sm-6 col-md-3 blog-post blog-post-grid">
-                        @elseif($key>0 && $key <=3 )
-                            <div class="offset-top-60 offset-sm-top-0 col-sm-6 col-md-3 blog-post blog-post-grid">
-                                @elseif($key>3)
-                                    <div class="offset-top-60 col-sm-6 col-md-3 blog-post blog-post-grid">
-                                        @endif
-                                        <div class="blog-post-media">
-                                            <a title="{{$media->name}}" rel="help" data-lightbox="image"
-                                               href="@if($media->qrcode){{asset($media->qrcode)}} @else {{asset($media->thumb)}} @endif"
-                                               class="thumbnail">
-                                                <img alt="{{$media->name}}"
-                                                     src="{{asset($media->thumb)}}"
-                                                     class="img-fluid">
-                                                <span class="caption"></span>
-                                            </a>
-                                        </div>
-                                        <div class="blog-post-meta">
-                                            <p>
-                                                @if($media->name)
-                                                    <span class='fst-italic'>Called </span>
-                                                    <a class="text-primary" href="{{url($url.'/'.$media->id)}}"
-                                                       target="_blank">
-                                                        {{$media->name}}
-                                                    </a> &#8226;
-                                                @endif
-                                                <span class='fst-italic'>View </span>
-                                                <a href="javascript:" data-bs-toggle="tooltip"
-                                                   data-bs-placement="right"
-                                                   title="质量度：{{ceil(abs($media->id - 17)/3)}}"
-                                                   class="ci-heart"></a>
-                                            </p>
-                                        </div>
-                                        @if(!stripos($media->url,'qingningzi'))
-                                            <p>
-                                                <a href="{{$media->url}}" title="{{$media->name}}"
-                                                   target="_blank" rel="nofollow">
-                                                    {{$media->name}}官网
-                                                </a>
-                                            </p>
-                                        @else
-                                            <p>{{$media->name}}</p>
-                                        @endif
-                                        <div><a href="{{url($url.'/'.$media->id)}}" class="btn btn-link"
-                                                target="_blank">
-                                                <span class="btn-text">查看更多</span>
-                                            </a>
-                                        </div>
-                                        <hr class="my-5">
-                                    </div>
-                                    @endforeach
-                                    <div class="text-center offset-top-45">
-                                        {{$data->render()}}
-                                    </div>
+            <div class="space-1-bottom">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb flex-lg-nowrap justify-content-center justify-content-lg-start">
+                        <li class="breadcrumb-item">
+                            <a href="{{ url('/') }}" class="text-nowrap" rel="nofollow">
+                                <i class="ci-home"></i> 首页
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item text-nowrap active">
+                            {{$title}}
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="row">
+                @foreach($data as $key=>$item)
+                    <div class="col-sm-12 col-md-3 mb-3">
+                        <div class="card border-1">
+                            <div class="card-img-top position-relative overflow-hidden">
+                                <a class="d-block" target="_blank" rel="nofollow" href="{{$item->url}}">
+                                    <img src="{{asset($item->thumb)}}" alt="{{$item->name}}" class="w-100">
+                                </a>
                             </div>
+                            <div class="badge bg-dark fs-sm position-absolute top-0 end-0 zindex-5 opacity-50">
+                                {{$item->name}}
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            {{$data->render()}}
+        </div>
 @endsection
