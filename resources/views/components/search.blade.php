01532ec1531d0310b1a88bea8f6502dbfc3cf5f4
cleveng <cleveng@gmail.com>
2023-08-13 11:43:20 +0800

    update search controller support tag keyword

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,5 +1,5 @@
-<div class="col-md-12 col-sm-12">
-    <h4 class="fw-normal">文章搜索</h4>
+<div class="widget widget-links mb-grid-gutter pb-grid-gutter border-bottom mx-lg-2">
+    <h3 class="widget-title">文章搜索</h3>
     <form action="https://www.so.com/s" id="so360form" autocomplete="off" method="GET" target="_blank">
         <div class="input-group">
             <label for="q so360_keyword" class="sr-only"></label>
@@ -9,7 +9,7 @@
             <input type="hidden" name="src" value="qingningzi.com"/>
             <input type="hidden" name="site" value="qingningzi.com"/>
             <input type="hidden" name="rg" value="1"/>
-            <button type="submit" id="so360_submit" class="btn btn-primary ms-1">搜一搜</button>
+            <button type="submit" id="so360_submit" class="btn btn-primary rounded-0 ms-1">搜一搜</button>
         </div>
     </form>
 </div>
