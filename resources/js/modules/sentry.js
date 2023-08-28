beae7c1fd0363ed43bb2bebbee8f1476e03020bf
cleveng <cleveng@gmail.com>
2023-08-28 23:33:17 +0800

    add .env arguments VITE_APP_ENV

--- resources/js/modules/sentry.js
+++ resources/js/modules/sentry.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/browser'
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (import.meta.env.DEV) return
+    if (import.meta.env.VITE_APP_ENV !== 'production') return
 
     Sentry.init({
         dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
