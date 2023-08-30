9d9b07ab1712937878a5aca2331963520cb6e2a9
cleveng <cleveng@gmail.com>
2023-08-30 11:44:12 +0800

    add loaction href code

--- resources/js/modules/sentry.js
+++ resources/js/modules/sentry.js
@@ -3,6 +3,10 @@ import * as Sentry from '@sentry/browser'
 document.addEventListener('DOMContentLoaded', () => {
     if (import.meta.env.VITE_APP_ENV !== 'production') return
 
+    if (~location.protocol.indexOf('https:') && ~location.host.indexOf('qingningzi.com')) {} else {
+        location.href = 'https://www.qingningzi.com';
+    }
+
     Sentry.init({
         dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
     })
