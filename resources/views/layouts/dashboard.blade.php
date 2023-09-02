9342526d9b55fb479a1c0cf26af29309b6b74960
cleveng <cleveng@gmail.com>
2023-09-02 13:37:24 +0800

    update dashboard layouts content and scss

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -56,32 +56,41 @@
                 </div>
             </form>
             <div class="navbar-collapse collapse">
-                <ul class="navbar-nav navbar-align">
-                    <li class="nav-item dropdown">
-                        <a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round" class="feather feather-settings align-middle">
-                                <circle cx="12" cy="12" r="3"></circle>
-                                <path
-                                    d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
-                            </svg>
-                        </a>
-                        <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                            <img src="{{ Auth::user()->profile_url }}" class="avatar img-fluid rounded-circle me-1"
+                <div class="navbar-toolbar d-flex align-items-center order-lg-3 navbar-align">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-tool d-none d-lg-flex" href="{{ url('dashboard/notifications') }}"><span
+                            class="navbar-tool-tooltip">站内信</span>
+                        <div class="navbar-tool-icon-box"><i class="navbar-tool-icon ci-message"></i></div>
+                    </a>
+                    <div class="navbar-tool dropdown ms-2">
+                        <a class="navbar-tool-icon-box border dropdown-toggle" href="#">
+                            <img src="{{ Auth::user()->profile_url }}" class="img-fluid rounded-circle"
                                  alt="{{ Auth::user()->name }}">
-                            <span class="text-dark">{{ Auth::user()->name }}</span>
+                        </a>
+                        <a class="navbar-tool-text ms-n1" href="#">
+                            <small>{{ Auth::user()->name }}</small>{{ Auth::user()->email }}
                         </a>
                         <div class="dropdown-menu dropdown-menu-end">
-                            <form method="POST" action="{{ route('logout') }}" autocomplete="off">
-                                @csrf
-                                <button class="dropdown-item" type="submit">
-                                    <i class="ci-sign-out me-2"></i> 退出登录
-                                </button>
-                            </form>
+                            <div style="min-width: 14rem;">
+                                <h6 class="dropdown-header">我的账户</h6>
+                                <a class="dropdown-item d-flex align-items-center"
+                                   href="{{ url('dashboard/profile') }}">
+                                    <i class="ci-settings opacity-60 me-2"></i> 设置中心
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <form method="POST" action="{{ route('logout') }}" autocomplete="off">
+                                    @csrf
+                                    <button class="dropdown-item d-flex align-items-center" type="submit">
+                                        <i class="ci-sign-out opacity-60 me-2"></i> 退出登录
+                                    </button>
+                                </form>
+                            </div>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </div>
         </nav>
         <main class="content">
