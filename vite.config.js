bc7ce5e5f57d4bc7bbf90c367825d2f874cd961f
cleveng <cleveng@gmail.com>
2023-09-02 13:12:19 +0800

    update used scss

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/scss/dashboard.scss', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/scss/dashboard.scss', 'resources/js/app.js', 'resources/js/dashboard.js'],
             refresh: true,
         }),
     ],
