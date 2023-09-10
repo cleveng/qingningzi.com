5d0c9ccaf0b979b8b7f543658d55c0aaae2f84b7
cleveng <cleveng@gmail.com>
2023-09-10 08:52:02 +0800

    add isDev variable for development, image use cdn

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,39 @@
 const {resolve} = require('path')
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
-export default defineConfig({
-    base: 'https://cdn.qingningzi.com/build/',
-    build: {
-        outDir: resolve(__dirname, 'public/build'),
-        emptyOutDir: true,
-        manifest: true,
-        target: 'es2018',
-    },
-    plugins: [
-        laravel({
-            input: ['resources/scss/app.scss', 'resources/scss/dashboard.scss', 'resources/js/app.js', 'resources/js/dashboard.js'],
-            refresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'resources'),
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+    const isDev = process.env.VITE_APP_ENV !== 'production'
+
+    return defineConfig({
+        base: isDev ? '.' : 'https://cdn.qingningzi.com/build/',
+        build: {
+            outDir: resolve(__dirname, 'public/build'),
+            emptyOutDir: true,
+            manifest: true,
+            target: 'es2018',
+        },
+        plugins: [
+            laravel({
+                input: ['resources/scss/app.scss', 'resources/scss/dashboard.scss', 'resources/js/app.js', 'resources/js/dashboard.js'],
+                refresh: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'resources'),
+            }
+        },
+        optimizeDeps: {
+            include: [
+                'bootstrap',
+                'feather-icons',
+                'smooth-scroll',
+                'axios',
+                'alpinejs',
+                'sentry',
+            ]
         }
-    },
-    optimizeDeps: {
-        include: [
-            "axios",
-        ]
-    }
-})
+    })
+}
