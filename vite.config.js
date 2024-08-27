05b5662d1d1f224864bc367b963fe16dbba3352c
cleveng <cleveng@gmail.com>
2024-08-27 09:33:19 +0800

    fix: tailwindcss use scss

--- vite.config.js
+++ vite.config.js
@@ -4,50 +4,40 @@ import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default ({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-    const isDev = process.env.VITE_APP_ENV !== 'production'
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  const isDev = process.env.VITE_APP_ENV !== 'production'
 
-    return defineConfig({
-        base: isDev ? './' : 'https://cdn.qingningzi.com/build/',
-        build: {
-            outDir: resolve(__dirname, 'public/build'),
-            emptyOutDir: true,
-            manifest: true,
-            target: 'es2018',
-            rollupOptions: {
-                output: {
-                    // 入口文件名
-                    entryFileNames: 'assets/[name].js',
-                    // 块文件名
-                    chunkFileNames: 'assets/[name].js',
-                    // 资源文件名 css 图片等等
-                    assetFileNames: 'assets/[name].[ext]',
-                }
-            }
-        },
-        plugins: [
-            laravel({
-                input: [
-                    'resources/scss/app.scss',
-                    'resources/js/main.js',
-                ],
-                refresh: true
-            })
-        ],
-        resolve: {
-            alias: {
-                '@': resolve(__dirname, 'resources')
-            }
-        },
-        optimizeDeps: {
-            include: [
-                'bootstrap',
-                'feather-icons',
-                'smooth-scroll',
-                'axios',
-                'alpinejs',
-                'sentry'
-            ]
+  return defineConfig({
+    base: isDev ? './' : 'https://cdn.qingningzi.com/build/',
+    build: {
+      outDir: resolve(__dirname, 'public/build'),
+      emptyOutDir: true,
+      manifest: true,
+      target: 'es2018',
+      rollupOptions: {
+        output: {
+          // 入口文件名
+          entryFileNames: 'assets/[name].js',
+          // 块文件名
+          chunkFileNames: 'assets/[name].js',
+          // 资源文件名 css 图片等等
+          assetFileNames: 'assets/[name].[ext]'
         }
-    })
+      }
+    },
+    plugins: [
+      laravel({
+        input: ['resources/scss/app.scss', 'resources/js/main.js'],
+        refresh: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'resources')
+      }
+    },
+    optimizeDeps: {
+      include: ['bootstrap', 'feather-icons', 'smooth-scroll', 'axios', 'alpinejs', 'sentry']
+    }
+  })
 }
